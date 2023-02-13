c20f88d51339f4f9e79550c2c0ba38299d8b0ff2
Laurielcio Freitas de Assis <laurielcio@yahoo.com.br>
2023-02-13 14:45:30 -0300

    Alteracação para apresentação

--- src/main/java/br/com/laurielcio/producer/service/impl/KafkaProducerServiceImpl.java
+++ src/main/java/br/com/laurielcio/producer/service/impl/KafkaProducerServiceImpl.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service("KafkaProducerService")
 public class KafkaProducerServiceImpl implements KafkaProducerService{
 	
-	private static final String TOPIC = "TRANSFERENCIA_TESTE";
+	private static final String TOPIC = "LAU_TOPIC";
 	
 	@Autowired
 	private KafkaTemplate<String, UsuarioDto> kafkaTemplate;
